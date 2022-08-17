58844e3a01ccd32e2e745e215f7d07c073bcdcb9
duong <duong.bui@opswaat.com>
2022-08-17 16:32:33 +0700

    finish learning useEffect

--- pages/learnUseEffect/index.js
+++ pages/learnUseEffect/index.js
@@ -1,3 +1,30 @@
-export default function index() {
-  return <div>index</div>;
+import { useEffect, useState } from "react";
+
+export default function useEffectLearning() {
+  const info = ["posts", "users", "comments"];
+  const [resType, setResType] = useState(info[0]);
+  const [contents, setContent] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resType}`)
+      .then((response) => response.json())
+      .then((json) => setContent(json));
+  }, [resType]);
+
+  return (
+    <div>
+      {/* <button onClick={() => setResType("posts")}>Posts</button>
+      <button onClick={() => setResType("users")}>Users</button>
+      <button onClick={() => setResType("comments")}>Comments</button> */}
+      {info.map((_info) => (
+        <button key={_info} onClick={() => setResType(_info)}>
+          {_info}
+        </button>
+      ))}
+      <div>{resType}</div>
+      {contents.map((content) => (
+        <pre key={JSON.stringify(content)}>{JSON.stringify(content)}</pre>
+      ))}
+    </div>
+  );
 }
